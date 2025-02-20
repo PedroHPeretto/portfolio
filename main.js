1203d705fad222b494a6744613e936dbd9eaaf5f
pedro <pedrohperetto@gmail.com>
2025-02-20 15:31:27 -0300

    first commit new portfolio

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#email').attr('placeholder', 'exemplo123@gmail.com')
+    $('#nome').attr('placeholder', 'Nome Sobrenome')
+    $('#mensagem').attr('placeholder', '"Vamos trabalhar juntos ?"')
+})
